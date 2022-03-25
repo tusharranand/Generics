f00441d8d18b1d1aa13a275134bec1c2f4f75ac0
Tushar Anand <tusharranand@gmail.com>
2022-03-25 13:29:49 +0530

    [Tushar Anand] Added: Introduction

---
+++ Generics_Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generics_Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum Problem using Generics");
+        }
+    }
+}
