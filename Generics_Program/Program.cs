70c5d6826de6d45d5f7f5ec4c0015026c93fa5a2
Tushar Anand <tusharranand@gmail.com>
2022-03-25 18:07:21 +0530

    [Tushar Anand] Added: Maximum of three floats

--- Generics_Program/Program.cs
+++ Generics_Program/Program.cs
@@ -7,13 +7,35 @@ namespace Generics_Program
         public static void Main(string[] args)
         {
             Console.WriteLine("Find Maximum Problem using Generics");
-            Console.Write("Enter the first value: ");
-            int First = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the second value: ");
-            int Second = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the third value: ");
-            int Third = Convert.ToInt32(Console.ReadLine());
-            Max_Integer max_Integer = new Max_Integer(First, Second, Third);
+            Console.WriteLine("Enter the number to compare");
+            Console.WriteLine("1 Integers");
+            Console.WriteLine("2 Floats");
+            int Control = Convert.ToInt32(Console.ReadLine());
+            switch (Control)
+            {
+                case 1:
+                    Console.Write("Enter the first value: ");
+                    int First_Int = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter the second value: ");
+                    int Second_Int = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter the third value: ");
+                    int Third_Int = Convert.ToInt32(Console.ReadLine());
+                    Max_Integer max_Integer = new Max_Integer(First_Int, Second_Int, Third_Int);
+                    break;
+                case 2:
+                    Console.Write("Enter the first value: ");
+                    float First_Flo = (float) Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter the second value: ");
+                    float Second_Flo = (float) Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter the third value: ");
+                    float Third_Flo = (float) Convert.ToDouble(Console.ReadLine());
+                    Max_Float max_Float = new Max_Float(First_Flo, Second_Flo, Third_Flo);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry");
+                    break;
+            }
+            
         }
     }
     public class Max_Integer
@@ -21,6 +43,14 @@ namespace Generics_Program
         public Max_Integer(int First, int Second, int Third)
         {
             int[] Sort = { First, Second, Third };
+            Console.WriteLine("\nThe maximum of the given values is " + Sort.Max());
+        }
+    }
+    public class Max_Float
+    {
+        public Max_Float(float First, float Second, float Third)
+        {
+            float[] Sort = { First, Second, Third };
             Console.WriteLine("\nThe maximum of the given values is " + Sort.Max());
         }
     }
