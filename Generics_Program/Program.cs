d9b56ba7fddfd9131b5774e1d2bf5e6181b444c8
Tushar Anand <tusharranand@gmail.com>
2022-03-25 17:43:29 +0530

    [Tushar Anand] Added: Maximum of three integers

--- Generics_Program/Program.cs
+++ Generics_Program/Program.cs
@@ -7,6 +7,21 @@ namespace Generics_Program
         public static void Main(string[] args)
         {
             Console.WriteLine("Find Maximum Problem using Generics");
+            Console.Write("Enter the first value: ");
+            int First = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second value: ");
+            int Second = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the third value: ");
+            int Third = Convert.ToInt32(Console.ReadLine());
+            Max_Integer max_Integer = new Max_Integer(First, Second, Third);
+        }
+    }
+    public class Max_Integer
+    {
+        public Max_Integer(int First, int Second, int Third)
+        {
+            int[] Sort = { First, Second, Third };
+            Console.WriteLine("\nThe maximum of the given values is " + Sort.Max());
         }
     }
 }
